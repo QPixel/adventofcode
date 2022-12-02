88dede4dfcfe68ffab3b593e25b64b9ef6fe2b3e
Riley Smith <riley@rileysmith.me>
2022-12-01 21:58:57 -0800

    day2 + some rusty bits

--- src/days/day1.ts
+++ src/days/day1.ts
@@ -1,12 +1,10 @@
 import { readFile } from "../utils/index.js";
 
 export async function day1(): Promise<void> {
-    const file = await readFile("day1.txt");
+    const file = await readFile("biginput.txt");
     const calories = file.split("\n\n").map((x) => x.split("\n")).map((x) => x.map(x => parseInt(x)));
-    const highest = calories.map((group) => group.reduce((a,b) => a+b)).reduce((a,b) => Math.max(a,b));
-    const topThree = calories.map((i) => {return i.reduce((a,b) => a+b)}).sort((a,b) => b-a).slice(0,3).reduce((a,b) => a+b) 
-    console.log(highest);
-    console.log(topThree)
+    const topThree = calories.map((i) => {return i.reduce((a,b) => a+b)}).sort((a,b) => b-a).slice(0,3);
+    console.log(topThree[0], topThree.reduce((a,b) => a+b));
     // let buf: number[] = [];
     // let calories: number[][] = []
     // for (let i in input) {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,5 +8,8 @@ export async function readFile(file_name: string) {
     const path = resolve(`${__dirname}/../../inputs/`, file_name);
     console.log(path);
     const file = await fs.readFile(path, "utf-8");
+    if (file[file.length - 1] === "\n" || file[file.length - 1] === "\r") {
+        return file.slice(0, -1);
+    }
     return file;
 }
