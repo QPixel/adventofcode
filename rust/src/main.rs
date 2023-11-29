35ab9691b6281cdbf5849eefa71c7c398bbd3f56
Riley Smith <riley@rileysmith.me>
2023-11-29 10:23:24 -0800

    Setup for 2023

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,3 @@
 use std::fs;
 
-fn main() {
-    let contents =
-        fs::read_to_string("../../inputs/biginput.txt").expect("Something went wrong reading the file");
-    let split: Vec<Vec<u32>> = contents.split("\n\n").map(|s| s.split("\n").map(|s| s.parse::<u32>().unwrap_or_default()).collect()).collect();
-    let mut sorted: Vec<u32> = split.iter().map(|n| n.iter().sum()).collect();
-    sorted.sort();
-    let top_three = sorted.iter().rev().take(3).collect::<Vec<&u32>>();
-    let sum = top_three.iter().map(|n| **n).sum::<u32>();
-    println!("{:?} {}", top_three[0], sum);
-}
+fn main() {}
--- src/days/day1.ts
+++ src/days/day1.ts
@@ -1,10 +1,7 @@
 import { readFile } from "../utils/index.js";
 
 export async function day1(): Promise<void> {
-    const file = await readFile("biginput.txt");
-    const calories = file.split("\n\n").map((x) => x.split("\n")).map((x) => x.map(x => parseInt(x)));
-    const topThree = calories.map((i) => {return i.reduce((a,b) => a+b)}).sort((a,b) => b-a).slice(0,3);
-    console.log(topThree[0], topThree.reduce((a,b) => a+b));
+    const file = await readFile("day1.txt");
 }
 
 await day1()
